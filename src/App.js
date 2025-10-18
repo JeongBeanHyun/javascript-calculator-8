f9451357e92048129c6e67d8f2ee3ed55f1558cc
JeongBeanHyun <kelly92108@gmail.com>
2025-10-19 06:15:32 +0900

    feat(app) : 덧셈 계산 함수 import 및 Console.print를 사용하여 계산 결과 출력 메시지 작성

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { MissionUtils } from "@woowacourse/mission-utils";
+import { add } from "./calculator.js";
 
 class App {
   async run() {
     const input = await MissionUtils.Console.readLineAsync(
       "덧셈할 문자열을 입력해 주세요.\n"
     );
+    const result = add(input);
+    MissionUtils.Console.print(`결과 : ${result}`);
   }
 }
 
