4bb8df59d6aeae263dfba334a9df022bf2d30c24
JeongBeanHyun <kelly92108@gmail.com>
2025-10-19 05:59:35 +0900

    feat(delimiters) : 구분자 기반 정규식 생성 함수 추가

--- src/delimiters.js
+++ src/delimiters.js
@@ -1,1 +1,9 @@
 export const DEFAULT_DELIMITERS = [",", ":"];
+
+export function buildDelimiterRegex(delimiters) {
+  const escaped = delimiters.map((d) =>
+    d.replace(/[.*+?^${}()|[\]\\]/g, "\\$&")
+  );
+  const regex = new RegExp(escaped.join("|"), "g");
+  return regex;
+}
